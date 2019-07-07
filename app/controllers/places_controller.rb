3e96eb9119689b1fec84729ef9d56d9487c316aa
Jessi Calicotte <orgkleen@gmail.com>
2019-07-07 18:46:02 +0000

    fixing error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,42 +1,52 @@
 class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-def index
-  @places = Place.all 
-end
-
-def new
-  @place = Place.new
+  def index
+    @places = Place.all 
   end
 
-def create
-current_user.places.create(place_params)
-redirect_to root_path
- end
+  def new
+    @place = Place.new
+  end
 
-def show
- @place = Place.find(params[:id])
-end
-def update
-   @place = Place.find(params[:id])
-  if @place.user != current_user
-return render plain: 'Not Allowed', status: :forbidden
- @place.update_attributes(place_params)
-  redirect_to root_path
+  def create
+    current_user.places.create(place_params)
+    redirect_to 
+  else
+    render :new, status: :unprocessable_entity
+  end
+ 
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def update
+     @place = Place.find(params[:id])
+    if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+    end
+
+      @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+  end
 end
 
 def destroy
-  @place = Place.find(params[:id])
-  if @place.user != current_user
+    @place = Place.find(params[:id])
+    if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
-  
-  @place.destroy
-  redirect_to root_path
+    @place.destroy
+    redirect_to root_path
 end
+
 def edit
-  @place = Place.find(params[:id])
-  if @place.user != current_user
+    @place = Place.find(params[:id])
+    if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
 end
@@ -48,4 +58,4 @@ end
     params.require(:place).permit(:name, :description, :address)
   end
 
-
+end

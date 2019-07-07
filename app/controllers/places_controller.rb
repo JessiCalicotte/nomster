30dbdcd734c4d594e0643669c461363c8305d4dd
Jessi Calicotte <orgkleen@gmail.com>
2019-07-07 17:34:11 +0000

    getting error on places_conroller.rb and fixing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -32,11 +32,10 @@ def destroy
 end
 def edit
   @place = Place.find(params[:id])
-
   if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
-end 
+end
 
 
   private
@@ -46,4 +45,3 @@ end
   end
 
 
-end
